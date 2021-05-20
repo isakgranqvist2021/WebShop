2d5257b49dc1999bd3cca8f2f2d0d7b459649a59
DustiasTheGuy <isakwebdev@gmail.com>
2021-05-20 14:42:34 +0200

    added app.set('env')

--- main.js
+++ main.js
@@ -15,15 +15,12 @@ const app = express(); // initialize express
 import index from './routers/index'; // every route that can be accessed without authorization
 import users from './routers/users'; // every route that can only be accessed with authorization
 
-import productMethods from './models/product/product.methods';
-productMethods.saveProduct();
-
 /* connect to mongodb */
 mongoose.connect(config.DB_URI, {
     useNewUrlParser: true, // current URL string parser is deprecated, and will be removed in a future version. 
     useUnifiedTopology: true, // Current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version.
     useCreateIndex: true
-}, function (err) { // check if an error has occured
+}, (err) => { // check if an error has occured
     if (err) // if error then exit the application
         process.exit(1);
 
@@ -62,6 +59,7 @@ app.use(express.urlencoded({
 /* view engine setup - responsible for the views folder */
 app.set('view engine', 'ejs'); // set ejs as the view engine
 app.set('views', path.join(path.dirname('./'), 'views')); // set the folder where the views will be stored
+app.set('env', process.env.NODE_ENV);
 app.use(expressEjsLayouts); // enable ejs layout - extend a single layout file
 
 
@@ -70,7 +68,7 @@ app.use('/public', express.static('public')); // set the folder where public fil
 app.use('/assets', express.static('node_modules/bootstrap')); // where bootstrap is loaded from
 
 
-app.use('*', configMiddleware.setupConfig);
+app.use(configMiddleware.setupConfig); // populates the config header with useful data
 
 
 /* router setup - responsible for the controllers folder */
@@ -78,6 +76,6 @@ app.use('/', authMiddleware.hasNotAuth, index); // every route that can be acces
 app.use('/users', authMiddleware.hasAuth, users); // every route that can only be accessed with authorization
 
 
-app.listen(config.PORT, function () { // start server and listen for incomming http requests
+app.listen(config.PORT, () => { // start server and listen for incomming http requests
     console.log('Server listening on', config.PORT);
 });
